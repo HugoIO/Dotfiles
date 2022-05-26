850e08be3836dad632d1a9309910262560031754
shaolin <BlissIO@protonmail.com>
2022-05-26 17:41:50 +0100

    new-old-dots

--- suckless-old/dwm/config.h
+++ suckless-old/dwm/config.h
@@ -1,4 +1,4 @@
-/* See LICENSE file for copyright and license details. */
+// This is shaolin's dwm config file feel free to use/copy/take anything from it ʕ •ᴥ•ʔ 
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -7,21 +7,29 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#808080";
-static const char col_gray3[]       = "#FFFFFF";
-static const char col_gray4[]       = "#000000";
-static const char col_cyan[]        = "#FFFFFF";
+static const char *fonts[]          = { "JetBrains Mono:size=10", "JoyPixels:pixelsize=30:anitalias=true:autohint=true", "Noto Cjk:pixelsize=30:anitalias=true:autohint=true" };
+static const char dmenufont[]       = "JetBrains Mono:size=10";
+
+static const char norm_fg[] = "#e0b1bc";
+static const char norm_bg[] = "#0B1827";
+static const char norm_border[] = "#9c7b83";
+
+static const char sel_fg[] = "#e0b1bc";
+static const char sel_bg[] = "#565D81";
+static const char sel_border[] = "#e0b1bc";
+
+static const char urg_fg[] = "#e0b1bc";
+static const char urg_bg[] = "#3F6683";
+static const char urg_border[] = "#3F6683";
+
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	    /*               fg           bg         border      */
+	    [SchemeNorm] = { norm_fg,     norm_bg,   norm_border },
+	    [SchemeSel]  = { sel_fg,      sel_bg,    sel_border },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4" };
+static const char *tags[] = { ">_", "'w'", "owo", "^_^" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -59,10 +67,10 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *nemo[]  = { "nemo", NULL };
-static const char *browser[]  = { "chromium", NULL };
+static const char *browser[]  = { "firefox", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -77,11 +85,11 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,	                XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
